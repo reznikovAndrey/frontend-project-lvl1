10eb1118eb9312513b11e1299456854534207def
Andrey Reznikov <reznikovandrusha@gmail.com>
2022-05-22 18:44:35 +0500

    Add user greeting
    - Ask user name
    - Greeting user

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import { question } from 'readline-sync';
+
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = question('May I have your name?');
+  console.log(`Hello, ${userName}!`);
+};
+
+export default greeting;
