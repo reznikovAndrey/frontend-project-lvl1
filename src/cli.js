8c25eb601d52663d103b8a48f53d8bc0937b6a30
Andrey Reznikov <reznikovandrusha@gmail.com>
2022-05-22 23:32:38 +0500

    Return username from cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,8 +2,9 @@ import { question } from 'readline-sync';
 
 const greeting = () => {
   console.log('Welcome to the Brain Games!');
-  const userName = question('May I have your name?');
+  const userName = question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+  return userName;
 };
 
 export default greeting;
